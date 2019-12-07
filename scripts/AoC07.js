1c475c590b207653419c41fa84e9b0a3ff708a8c
Martin Wildeman <martin.wildeman@onegini.com>
2019-12-07 20:48:47 +0100

    Remove wrong HTML page

--- scripts/AoC07.js
+++ scripts/AoC07.js
@@ -1,5 +1,5 @@
-class Program{
-    constructor (code, phase, finalNode) {
+class Program {
+    constructor(code, phase, finalNode) {
         this.code = code.split(',').map(Number);
         this.inputLocation = 0;
         this.pointer = 0;
@@ -8,12 +8,12 @@ class Program{
         this.input = [phase];
         this.finalNode = finalNode;
     }
-    run(inputValue){
+    run(inputValue) {
         this.input.push(inputValue);
         while (this.pointer < 1000) {
             var instruction = this.code[this.pointer];
             var A = 0, B = 0, C = 0, opCode = 0;
-    
+
             if (instruction >= 10000) {
                 //How can this ever be true? It's never used...
                 instruction -= 10000;
@@ -28,11 +28,11 @@ class Program{
                 C = 1;
             }
             opCode = instruction;
-    
+
             if (A) {
                 debugger;
             }
-    
+
             //Parameters that an instruction writes to will never be in immediate mode.
             switch (opCode) {
                 case 1:
@@ -56,7 +56,7 @@ class Program{
                     this.output = C ? this.code[this.pointer + 1] : this.code[this.code[this.pointer + 1]];
                     //console.log("Output: %d",this.output);
                     this.pointer += 2; // one parameter
-                    if(this.consumer){
+                    if (this.consumer) {
                         //Start the next node;
                         this.consumer.run(this.output);
                     }
@@ -89,10 +89,10 @@ class Program{
                     this.code[this.code[this.pointer + 3]] = (C ? this.code[this.pointer + 1] : this.code[this.code[this.pointer + 1]]) === (B ? this.code[this.pointer + 2] : this.code[this.code[this.pointer + 2]]) ? 1 : 0;
                     this.pointer += 4; // three parameters
                     break;
-    
+
                 case 99:
                     //console.log('Exit code: %d.',output);
-                    if(this.finalNode)
+                    if (this.finalNode)
                         return this.output;
                     return this.consumer.run(this.output);
                 default:
@@ -101,60 +101,55 @@ class Program{
             }
         }
     }
-    setConsumer(consumer){
+    setConsumer(consumer) {
         this.consumer = consumer;
     }
-} 
+}
 
-function runAmplifiers(code, phases){
-    var output = 0;
+function runAmplifiers(code, phases) {
     var amplifiers = []
-    for(var i =0;i<phases.length;i++)
-    {
-        var amp = new Program(code,phases[i],i==4)
+    for (var i = 0; i < phases.length; i++) {
+        var amp = new Program(code, phases[i], i == 4)
         amplifiers.push(amp);
-        if(i>0){
-            amplifiers[i-1].setConsumer(amp);
+        if (i > 0) {
+            amplifiers[i - 1].setConsumer(amp);
         }
-        if(i==4)
-        {
+        if (i == 4) {
             amp.setConsumer(amplifiers[0])
         }
     }
     return amplifiers[0].run(0);
 };
 
-function toArray(input){
-    if(typeof input==='Array')
-    {
+function toArray(input) {
+    if (typeof input === 'Array') {
         return input;
     }
     return [input];
 }
 
-function getAllPossibilities(array){
+function getAllPossibilities(array) {
     var possibilities = [];
 
-    var fn = function (current,leftArray){
-        if(leftArray.length===0)
-        {
+    var fn = function (current, leftArray) {
+        if (leftArray.length === 0) {
             possibilities.push(current);
             return;
         }
-        for(var i=0;i<leftArray.length;i++){
+        for (var i = 0; i < leftArray.length; i++) {
             var newLeftArray = [...leftArray];
             var newCurrent = [...current];
-            var newItem = newLeftArray.splice(i,1);
+            var newItem = newLeftArray.splice(i, 1);
             newCurrent = newCurrent.concat(newItem);
-            fn(newCurrent,newLeftArray);
+            fn(newCurrent, newLeftArray);
         }
     }
 
-    fn([],array)
-    
+    fn([], array)
+
     return possibilities;
 }
-var possibilities = getAllPossibilities([0,1,2,3,4],[]);
+var possibilities = getAllPossibilities([0, 1, 2, 3, 4], []);
 
 // //Test 1:
 // var code = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0";
@@ -179,9 +174,9 @@ var startTime = Date.now();
 //Puzzle part 1:
 console.log('Part 1:')
 var code = "3,8,1001,8,10,8,105,1,0,0,21,30,51,72,81,94,175,256,337,418,99999,3,9,101,5,9,9,4,9,99,3,9,1001,9,3,9,1002,9,2,9,1001,9,2,9,1002,9,5,9,4,9,99,3,9,1002,9,4,9,101,4,9,9,102,5,9,9,101,3,9,9,4,9,99,3,9,1002,9,4,9,4,9,99,3,9,102,3,9,9,1001,9,4,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,99";
-var results = possibilities.map(phases => runAmplifiers(code,phases));
+var results = possibilities.map(phases => runAmplifiers(code, phases));
 var max = Math.max(...results);
-console.log('Max: %c%d', 'font-weight: bold; color: #ff0000',max);
+console.log('Max: %c%d', 'font-weight: bold; color: #ff0000', max);
 
 
 var millis = Date.now() - startTime;
@@ -189,7 +184,7 @@ console.log(`Milliseconds elapsed = ${millis}`);
 
 
 
-var possibilities = getAllPossibilities([5,6,7,8,9],[]);
+var possibilities = getAllPossibilities([5, 6, 7, 8, 9], []);
 
 // //Test 1:
 // var code = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5";
@@ -207,12 +202,12 @@ var possibilities = getAllPossibilities([5,6,7,8,9],[]);
 var startTime = Date.now();
 
 //Puzzle part 2:
-console.log('%c=============================','color: #cccccc;');
+console.log('%c=============================', 'color: #cccccc;');
 console.log('Part 2:');
 var code = "3,8,1001,8,10,8,105,1,0,0,21,30,51,72,81,94,175,256,337,418,99999,3,9,101,5,9,9,4,9,99,3,9,1001,9,3,9,1002,9,2,9,1001,9,2,9,1002,9,5,9,4,9,99,3,9,1002,9,4,9,101,4,9,9,102,5,9,9,101,3,9,9,4,9,99,3,9,1002,9,4,9,4,9,99,3,9,102,3,9,9,1001,9,4,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,99";
-var results = possibilities.map(phases => runAmplifiers(code,phases));
+var results = possibilities.map(phases => runAmplifiers(code, phases));
 var max = Math.max(...results);
-console.log('Max: %c%d', 'font-weight: bold; color: #ff0000',max);
+console.log('Max: %c%d', 'font-weight: bold; color: #ff0000', max);
 
 
 var millis = Date.now() - startTime;
