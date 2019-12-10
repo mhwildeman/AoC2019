0f87e086e872790300049d9804ad5c6ed92d585c
Martin Wildeman <mhwildeman@gmail.com>
2019-12-10 12:23:47 +0100

    Day 10, part 2

--- scripts/AoC10.js
+++ scripts/AoC10.js
@@ -1,6 +1,9 @@
 class Map {
     constructor(mapinput) {
         this.map = mapinput.split(',').map(x => x.split(''));
+        this.initialize();
+    }
+    initialize() {
         this.asteroids = [];
         this.populateAsteroids();
         this.calculateSightings();
@@ -10,7 +13,7 @@ class Map {
         for (var y = 0; y < this.map.length; y++) {
             for (var x = 0; x < this.map[y].length; x++) {
                 if (this.map[y][x] === '#') {
-                    this.asteroids.push(new Asteroid(x,y));
+                    this.asteroids.push(new Asteroid(x, y));
                 }
             }
         }
@@ -18,16 +21,15 @@ class Map {
     calculateSightings = function () {
         this.asteroids.forEach(asteroidSrc => {
             this.asteroids.forEach(asteroidDst => {
-                asteroidSrc.addSighting(asteroidDst.x,asteroidDst.y);
+                asteroidSrc.addSighting(asteroidDst);
             });
         });
     }
     getMaxSightings = function () {
         var max = 0;
         var maxAst = null
-        this.asteroids.forEach(asteroid =>{
-            if(max<asteroid.getSightingCount())
-            {
+        this.asteroids.forEach(asteroid => {
+            if (max < asteroid.getSightingCount()) {
                 max = asteroid.getSightingCount();
                 maxAst = asteroid;
             }
@@ -42,64 +44,120 @@ class Asteroid {
         this.y = y;
         this.sightings = []; //this field contains sightings.
     }
-    addSighting = function (x, y) {
-        var label;
-        if (x < this.x) {
-            label = 'n'; //North
-        }
-        else {
-            label = 's'; //South
-        }
+    addSighting = function (addedAsteroid) {
+        var x = addedAsteroid.x;
+        var y = addedAsteroid.y;
+        var degOffset;
+        var degrees;
         switch (true) {
             case x === this.x && y === this.y:
                 //We're us. Stop right here.
                 return;
             case x === this.x:
                 //We're on the vertical line
-                label += y < this.y ? 'n' : 's';
+                degrees = y < this.y ? 0 : 180;
                 break;
             case y === this.y:
-                label += x < this.x ? 'w' : 'e';
+                degrees = x < this.x ? 270 : 90;
                 break;
-            default:
-                label += (Math.round(1000 * ((x - this.x) / (y - this.y))));
+            case y < this.y && x > this.x: //We're north-east. No offset.
+                degOffset = 0;
+                var o = Math.abs(x - this.x);
+                var a = Math.abs(y - this.y)
+                degrees = degOffset + Math.atan(o / a) * 180 / Math.PI;
                 break;
-
+            case y > this.y && x > this.x: //We're south-east.
+                degOffset = 90;
+                var o = Math.abs(y - this.y)
+                var a = Math.abs(x - this.x);
+                degrees = degOffset + Math.atan(o / a) * 180 / Math.PI;
+                break;
+            case y > this.y && x < this.x: //We're south-west.
+                degOffset = 180;
+                var a = Math.abs(y - this.y)
+                var o = Math.abs(x - this.x);
+                degrees = degOffset + Math.atan(o / a) * 180 / Math.PI;
+                break;
+            case y < this.y && x < this.x: //We're south-west.
+                degOffset = 270;
+                var o = Math.abs(y - this.y)
+                var a = Math.abs(x - this.x);
+                degrees = degOffset + Math.atan(o / a) * 180 / Math.PI;
+                break;
         }
+
+        var label = '' + Math.round(degrees * 1000);
 
         //Now all astroids on the same line from `this` will get same label.
-        if (_.indexOf(this.sightings, label) === -1) {
-            this.sightings.push(label);
+        var ast = _.find(this.sightings, function (asteroid) { return asteroid.label === label });
+        if (typeof ast === 'undefined') {
+            this.sightings.push({ label: label, count: 1, degrees: degrees, ast: [addedAsteroid] });
+        }
+        else {
+            ast.count += 1;
+            ast.ast.push(addedAsteroid);
+            //Make sure that first asteroid is closest.
+            ast.ast = _.sortBy(ast.ast, function (ast) {
+                return Math.sqrt(Math.pow(ast.y - y, 2) + Math.pow(ast.x - x, 2));
+            });
         }
     }
-    getSightingCount(){return this.sightings.length;}
+    shootSightings(count) {
+        this.sightings = _.sortBy(this.sightings, 'degrees');
+        var vaporized = 0;
+        if (count <= this.sightings.length) {
+            //We need less than one turn. No need to complicate stuff.
+            return this.sightings[count - 1].ast[0];
+        }
+
+        //This part is not needed for puzzle, but who knows. Maybe we'll need it some time.
+        while (count > 0) {
+            var asteroid = null;
+            for (var i = 0; i < this.sightings.length; i++) {
+                if (this.sightings[i].count > 0) {
+                    this.sightings[i].count--;
+                    asteroid = this.sightings[i].ast.shift();
+                    count--;
+                }
+                if (count === 0) {
+                    //We've made more than one turn and are done.
+                    return asteroid;
+                }
+
+            }
+        }
+    }
+    getSightingCount() { return this.sightings.length; }
 }
 
 
-//Test 1
-var mapInput = '.#..#,.....,#####,....#,...##'
-var map = new Map(mapInput);
-console.log(map.getMaxSightings());
+// //Test 1
+// var mapInput = '.#..#,.....,#####,....#,...##'
+// var map = new Map(mapInput);
+// console.log(map.getMaxSightings());
 
-//Test 2
-var mapInput = '......#.#.,#..#.#....,..#######.,.#.#.###..,.#..#.....,..#....#.#,#..#....#.,.##.#..###,##...#..#.,.#....####';
-var map = new Map(mapInput);
-console.log(map.getMaxSightings());
+// //Test 2
+// var mapInput = '......#.#.,#..#.#....,..#######.,.#.#.###..,.#..#.....,..#....#.#,#..#....#.,.##.#..###,##...#..#.,.#....####';
+// var map = new Map(mapInput);
+// console.log(map.getMaxSightings());
 
-//Test 3
-var mapInput = '#.#...#.#.,.###....#.,.#....#...,##.#.#.#.#,....#.#.#.,.##..###.#,..#...##..,..##....##,......#...,.####.###.';
-var map = new Map(mapInput);
-console.log(map.getMaxSightings());
+// //Test 3
+// var mapInput = '#.#...#.#.,.###....#.,.#....#...,##.#.#.#.#,....#.#.#.,.##..###.#,..#...##..,..##....##,......#...,.####.###.';
+// var map = new Map(mapInput);
+// console.log(map.getMaxSightings());
 
-//Test 4
-var mapInput = '.#..#..###,####.###.#,....###.#.,..###.##.#,##.##.#.#.,....###..#,..#.#..#.#,#..#.#.###,.##...##.#,.....#.#..';
-var map = new Map(mapInput);
-console.log(map.getMaxSightings());
+// //Test 4
+// var mapInput = '.#..#..###,####.###.#,....###.#.,..###.##.#,##.##.#.#.,....###..#,..#.#..#.#,#..#.#.###,.##...##.#,.....#.#..';
+// var map = new Map(mapInput);
+// console.log(map.getMaxSightings());
 
-//Test 5
-var mapInput = '.#..##.###...#######,##.############..##.,.#.######.########.#,.###.#######.####.#.,#####.##.#.##.###.##,..#####..#.#########,####################,#.####....###.#.#.##,##.#################,#####.##.###..####..,..######..##.#######,####.##.####...##..#,.#####..#.######.###,##...#.##########...,#.##########.#######,.####.#.###.###.#.##,....##.##.###..#####,.#.#.###########.###,#.#.#.#####.####.###,###.##.####.##.#..##';
-var map = new Map(mapInput);
-console.log(map.getMaxSightings());
+// //Test 5
+// var mapInput = '.#..##.###...#######,##.############..##.,.#.######.########.#,.###.#######.####.#.,#####.##.#.##.###.##,..#####..#.#########,####################,#.####....###.#.#.##,##.#################,#####.##.###..####..,..######..##.#######,####.##.####...##..#,.#####..#.######.###,##...#.##########...,#.##########.#######,.####.#.###.###.#.##,....##.##.###..#####,.#.#.###########.###,#.#.#.#####.####.###,###.##.####.##.#..##';
+// var map = new Map(mapInput);
+// var station = map.getMaxSightings();
+// console.log(station)
+// var astroidShot = station.shootSightings(200)
+// console.log(astroidShot.x * 100 + astroidShot.y);
 
 //Puzzle 1
 var startTime = Date.now();
@@ -108,6 +166,10 @@ var startTime = Date.now();
 console.log('Part 1:')
 var mapInput = '#.....#...#.........###.#........#..,....#......###..#.#.###....#......##,......#..###.......#.#.#.#..#.......,......#......#.#....#.##....##.#.#.#,...###.#.#.......#..#...............,....##...#..#....##....#...#.#......,..##...#.###.....##....#.#..##.##...,..##....#.#......#.#...#.#...#.#....,.#.##..##......##..#...#.....##...##,.......##.....#.....##..#..#..#.....,..#..#...#......#..##...#.#...#...##,......##.##.#.#.###....#.#..#......#,#..#.#...#.....#...#...####.#..#...#,...##...##.#..#.....####.#....##....,.#....###.#...#....#..#......#......,.##.#.#...#....##......#.....##...##,.....#....###...#.....#....#........,...#...#....##..#.#......#.#.#......,.#..###............#.#..#...####.##.,.#.###..#.....#......#..###....##..#,#......#.#.#.#.#.#...#.#.#....##....,.#.....#.....#...##.#......#.#...#..,...##..###.........##.........#.....,..#.#..#.#...#.....#.....#...###.#..,.#..........#.......#....#..........,...##..#..#...#..#...#......####....,.#..#...##.##..##..###......#.......,.##.....#.......#..#...#..#.......#.,#.#.#..#..##..#..............#....##,..#....##......##.....#...#...##....,.##..##..#.#..#.................####,##.......#..#.#..##..#...#..........,#..##...#.##.#.#.........#..#..#....,.....#...#...#.#......#....#........,....#......###.#..#......##.....#..#,#..#...##.........#.....##.....#....';
 var map = new Map(mapInput);
-console.log(map.getMaxSightings());
+var station = map.getMaxSightings();
+console.log(station)
+console.log('Part 2:')
+var astroidShot = station.shootSightings(200)
+console.log(astroidShot.x * 100 + astroidShot.y);
 var millis = Date.now() - startTime;
 console.log(`Milliseconds elapsed = ${millis}`);
